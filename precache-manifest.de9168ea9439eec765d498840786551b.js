83f06ca1f203113c49fb107b241f28611c2f3a1d
Simon <simonfong6@gmail.com>
2020-05-16 04:39:02 -0700

    Updates

---
+++ precache-manifest.de9168ea9439eec765d498840786551b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16afb4d1ed2cfe4c8744db508d9ff7d8",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "a45e0fba7fd01260c9ff",
+    "url": "/react-gh-pages/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "58f1e1553acf802bccc0",
+    "url": "/react-gh-pages/static/js/2.34d74ee6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-gh-pages/static/js/2.34d74ee6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a45e0fba7fd01260c9ff",
+    "url": "/react-gh-pages/static/js/main.0b8cb291.chunk.js"
+  },
+  {
+    "revision": "de9ecbc8f63a94727119",
+    "url": "/react-gh-pages/static/js/runtime-main.af17ebc1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2acb77129bffd3272ca2c72fc58e68a1.js"
+  "/react-gh-pages/precache-manifest.de9168ea9439eec765d498840786551b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-gh-pages/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
