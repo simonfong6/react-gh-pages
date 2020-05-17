6cba3799ed9fda730a85bd8b4e958be42e46b42c
Simon <simonfong6@gmail.com>
2020-05-16 18:49:27 -0700

    Add image

--- app/src/components/Drink/Drink.js
+++ app/src/components/Drink/Drink.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './Drink.css';
+import image from './wushiland-jasmine-green-milk-tea-tiny-boba.jpg';
 
 function Drink() {
   return (
-    <div className="Drink">
-      boba
+    <div>
+      <div className="drink border">
+        <img className="drink-thumbnail" src={image} />
+        <h2 className="">Jasmine Green Milk Tea</h2>
+        boba
+      </div>
     </div>
   );
 }
