c4d3db2ecc2b101dfa607c8539f3a9f944e3dadf
Simon <simonfong6@gmail.com>
2020-05-16 17:11:36 -0700

    Dirnk

---
+++ app/src/components/Drink/Drink.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Drink.css';
+
+function Drink() {
+  return (
+    <div className="Drink">
+      boba
+    </div>
+  );
+}
+
+export default Drink;
---
+++ app/src/components/Drink/index.js
@@ -0,0 +1,1 @@
+export {default as Drink} from './Drink.js';
